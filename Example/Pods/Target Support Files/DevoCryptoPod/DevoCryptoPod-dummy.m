a710541bdb9c24ec3c2d44bd47301b5112234588
Sebastien Aubin <spicheaubin@devolutions.net>
2020-04-21 16:51:56 -0400

    First commit

---
+++ Example/Pods/Target Support Files/DevoCryptoPod/DevoCryptoPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DevoCryptoPod : NSObject
+@end
+@implementation PodsDummy_DevoCryptoPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-DevoCryptoPod_Tests/Pods-DevoCryptoPod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevoCryptoPod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DevoCryptoPod_Tests
+@end
